4ebd535a7e02fb9fc736b8d4fcbf914734d1a5af
Nicco <niccologwyne@gmail.com>
2025-11-03 21:02:26 +0800

    created typescript for the timeslot

---
+++ product/src/app/model/time-slot.ts
@@ -0,0 +1,8 @@
+export class TimeSlot {
+    id: number = 0;
+    slotDate: string = '';
+    slotTime: string = '';
+    therapistId: number = 0;
+    therapistName: string = '';
+    isAvailable: boolean = true;
+}
